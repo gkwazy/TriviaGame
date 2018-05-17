c4904d389d194eb11635ce8bc1bd6d7d95475639
gkwazy <noreply>
2018-05-17 14:24:15 -0600

    geting closer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,14 @@
-var countDown = 20;
+var countDown = 5;
 var usersChoice = [false, false, false, false];
+var numberCorrect = 0;
 var colorQuestion = new question("What is my favorite color?", "blue no yellow", "red", "purple", "black like my soul");
+var strongestAvenger = new question("Who is the strongest avenger?", "thor", "batman", "hulk", "thanos");
+var dna = new question("what does DNA stand for?", "Deoxyribonuleic acid", "Data numeric architecture", "dihydrogen nitirc acid", "Danina's nice aadvark")
+var arrayOfQuestions = [colorQuestion, strongestAvenger, dna];
+var questionTheUserIsOn = arrayOfQuestions.length - 1;
 
-startGame();
-
-conclusionToQuestion();
+shuffleArray(arrayOfQuestions);
+startRound(arrayOfQuestions[questionTheUserIsOn]);
 
 $("#firstAnswer").on("click", function () {
     buttonSelected(0)
@@ -22,39 +26,53 @@ $("#fourthAnswer").on("click", function () {
     buttonSelected(3)
 });
 
-function startGame() {
-    var myTimer = setInterval(() => {
 
+function startRound(array) {
+    console.log("round started")
+    var roundArray = [array.correctAnswer, array.firstIncorrectAnswer, array.secondIncorrectAnswer, array.thirdIncorrectAnswer];
+    shuffleArray(roundArray);
+    console.log(array);
+    console.log(roundArray)
+    var myTimer = setInterval(() => {
         countDownTimer();
         console.log("myTimer called")
         if (countDown == 0) {
             clearInterval(myTimer)
-            conclusionToQuestion();
+            endOfRound(roundArray);
         }
         console.log(usersChoice);
     }, 1000);
-    document.getElementById("theQuestion").innerHTML = colorQuestion.question;
-    document.getElementById("firstAnswer").innerHTML = colorQuestion.correctAnswer;
-    document.getElementById("secondAnswer").innerHTML = colorQuestion.firstIncorrectAnswer;
-    document.getElementById("thirdAnswer").innerHTML = colorQuestion.secondIncorrectAnswer;
-    document.getElementById("fourthAnswer").innerHTML = colorQuestion.thirdIncorrectAnswer;
+
+    document.getElementById("theQuestion").innerHTML = array.question;
+    document.getElementById("firstAnswer").innerHTML = roundArray[0][0];
+    document.getElementById("secondAnswer").innerHTML = roundArray[1][0];
+    document.getElementById("thirdAnswer").innerHTML = roundArray[2][0];
+    document.getElementById("fourthAnswer").innerHTML = roundArray[3][0];
 
 }
+
+function endOfRound(array) {
+    for (var i = 0; i < array; i++) {
+        if (array[i][1] == true && usersChoice == true)
+            numberCorrect++;
+    }
+    if (questionTheUserIsOn >= 0) {
+        questionTheUserIsOn--;
+        countDown = 6;
+        startRound(arrayOfQuestions[questionTheUserIsOn]);
+    } else {
+        $('#myModal').on('shown.bs.modal', function () {
+            $('#myInput').trigger('focus')
+        })
+
+    }
+}
+
 function countDownTimer() {
     countDown--;
     document.getElementById("timer").innerHTML = countDown;
 
 };
-
-function conclusionToQuestion() {
-    for (var i = 0; i < userChoice.length; i++) {
-        if (usersChoice == true) {
-            if (userChoice) {
-
-            }
-        }
-    }
-}
 
 function buttonSelected(usersButton) {
     for (var i = 0; i < usersChoice.length; i++) {
@@ -69,9 +87,19 @@ function buttonSelected(usersButton) {
 function question(question, correctAnswer, firstIncorrectAnswer,
     secondIncorrectAnswer, thirdIncorrectAnswer) {
     this.question = question;
-    this.correctAnswer = correctAnswer;
-    this.firstIncorrectAnswer = firstIncorrectAnswer;
-    this.secondIncorrectAnswer = secondIncorrectAnswer;
-    this.thirdIncorrectAnswer = thirdIncorrectAnswer;
+    this.correctAnswer = [correctAnswer, true];
+    this.firstIncorrectAnswer = [firstIncorrectAnswer, false];
+    this.secondIncorrectAnswer = [secondIncorrectAnswer, false];
+    this.thirdIncorrectAnswer = [thirdIncorrectAnswer, false];
+
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
 
